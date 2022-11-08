82927aa16942943ed02de3987b786c8b205c2af2
junior-brempong <juniorwilliam222@gmail.com>
2022-11-08 16:17:27 -0500

    Added branch for Junior and Rylan :dancers:

---
+++ script.js
@@ -0,0 +1,16 @@
+// Phase 1: Recieving the results:
+
+// a: Create a variable to store our form in --> query selector?
+// b: Create a listner so we know when the user has submitted their choices
+// c: prevent default()
+// d: Store users choices in a variable
+// e: Now that we've got our users selection in the form of a string, lets use it to target the appropriate array with the drinks objects
+// f: Create a randomizer function for our drinks array --> this will produce one drink option in the form of a string 
+
+
+// Phase 2: Make a call to the cocktail API (this will happen in the event listener function once the user submits)
+
+// a: based off of the users drink option we have saved in the form of a string call the api to get back the image & title/header based on the users choice
+// b: display those results to the page
+    // b(a):add all the content we have dynamicallly created to the ul through its id name which already exists in the html
+
