220b89f6494392871587010e177867f9e4f84408
rylankearney <rylan.isabel@live.ca>
2022-11-14 10:04:02 -0500

    cleaned up comments :raised_hands:

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
-// Code for pop-up to display!
+//GOAL: The goal for our app is to collect a list of cocktails containing the user's choice of spirit, and provide them with a list of cocktails containing that spirit from the Cocktail Database API
 
 
-// Phase 1: General Set Up:
-
-// a: Make Name Space
+//Step 1: Making a namespace:
 
 const drinksApp = {};
 
-// // 2(a): Store the key (if needed)
-// drinksApp.key = `1`;
 
-// Store the users selection in a global variable to access
-
-
-
-
-// Phase 2: Function for calling the API:
+//Step 2: Function for calling the API:
 
 drinksApp.getDrink = (query) => {
     
     
-    // b: Store url = new URL(Endpoint)
+    // 2a: Store url = new URL(Endpoint)
     const url = new URL(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${query}`);
 
 
-    // c: Fetch(url) 
+    // 2b: Fetch(url) 
 
     fetch(url)
 
-    // d: .then() x 2 
+    // 2c: .then() x 2 
 
     .then(res => {
         return res.json()
@@ -36,7 +27,7 @@ drinksApp.getDrink = (query) => {
     .then(data => {
         const array = data.drinks;
         const first10 = array.slice(0, 10);
-        console.log(first10);
+        //console.log(first10);
         // empty out what is currently in the ul
         document.querySelector(`#drinkDisplay`).innerHTML = ``;
         // display the content
@@ -45,9 +36,9 @@ drinksApp.getDrink = (query) => {
 
 }
 
-// Phase 3: Displaying the art to the page:
+//Step 3: Displaying the drinks to the page:
 
-// a: Start function
+// 3a: Start function
 
     drinksApp.displayDrinks = (cocktails) => {
         
@@ -77,30 +68,26 @@ drinksApp.getDrink = (query) => {
     }
 
 
-// Phase 4: Getting the input
+// Step 4: Getting the input
 
 
     drinksApp.getUserInput = () => {
-        //console.log("hi")
         const choice = document.querySelector(`#spirit`);
         choice.addEventListener(`change`, function() {
             const selection = this.value
-            console.log(selection);
+            //console.log(selection);
             drinksApp.getDrink(selection);
-            //location.reload();
-
-                    
         });
     }
 
 
 
-// Phase 1(b):
+// Clickable pop up form to retrieve user's selection:
 drinksApp.setUpEventListener = () => {
     console.log("jkdshfkjds") 
     const findButton = document.querySelector("#find");
     findButton.addEventListener("click", function(){
-        console.log("hello")
+        //console.log("hello")
         document.querySelector(".menu").classList.add("active");
     });
     
@@ -110,21 +97,16 @@ drinksApp.setUpEventListener = () => {
     
 }
 
-// Make init method that will store our code/functions that need to run on page load
+//Step 5: Make init method that will store our code/functions that need to run on page load
 
 drinksApp.init = () => {
-    console.log("ready to go!");
+    //console.log("ready to go!");
     drinksApp.setUpEventListener();
-
     drinksApp.getUserInput();
-    //drinksApp.getDrink();
-    
-    //drinksApp.displayDrinks();
 }
 
 
-
-// Call init method
+//Step 6: Call init method
 
 drinksApp.init()
 
