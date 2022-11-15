b54d64f267b7df6c33998d0f1530a26c63d5bfa5
junior-brempong <juniorwilliam222@gmail.com>
2022-11-15 10:29:40 -0500

    Added in modules for stretch goal!

---
+++ scripts/searchFunction.js
@@ -0,0 +1,20 @@
+// Search function for the ingredient section
+
+// Step 1: Creating modules
+
+// 1(a)Setup Import/Export modules with script.js and index.html
+
+// Step 2: Calling the API
+// 2(a) Create a function and store our endpoint
+// 2(b) Fetch url
+// 2(c) .Then, where we will recieve our JSON object and parse through to recieve our array of objects which we can parse through to grab the ingredient key
+
+// Step 3: Get the user input
+// 3(a) Add an event listener to our input form and pass through the submit action where we will gather the value of the users submission. We can then take this value and call our display drinks function and pass through the users submission value.
+
+
+// Step 4: Displaying our drinks to the page
+// 4(a) Start a function & create a .forEach method where we can create a new element for every time the user clicks submit
+// 4(b) Create elements for li>h2>img elements
+// 4(c) Append elements h2 & img (with src & alt) to our li
+// 4(d) Append the dynamic li to our UL element which currently exists in our HTML
